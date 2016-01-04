efb975f05e15997d1e198018d33518d301e8944d
Esphas <esphas@hotmail.com>
2016-01-04 19:48:30 +0800

    [skip ci] Added few docs

--- lib/rayormoche/application.rb
+++ lib/rayormoche/application.rb
@@ -1,11 +1,15 @@
 ##
 # Rayormoche Application
+#
+# An application behaves similarly to a command, so Rayormoche::Application takes Rayormoche::Command as superclass.
+# Unlike normal Command, an Application does not have parent command,
+# which means what does an Application name does not really matter.
+# For more details, see Rayormoche::Command.
 class Rayormoche::Application < Rayormoche::Command
 
   ##
   # Initialize an application with a name
   def initialize appname
     super appname, nil
-    # TODO:
   end
 end
